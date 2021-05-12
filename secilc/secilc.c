ee293586032526abc87956df1c3a8d51b03c16b6
Christian Göttsche <cgzones@googlemail.com>
2021-05-12 15:50:08 +0200

    secilc: improve error reporting
    Be more verbose by printing the underlying OS error.
    Print related file names.
    Check fclose(3) on output files, cause a failure can be caused by a
    write/sync failure.
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

--- secilc/secilc.c
+++ secilc/secilc.c
@@ -267,13 +267,13 @@ int main(int argc, char *argv[])
 	for (i = optind; i < argc; i++) {
 		file = fopen(argv[i], "r");
 		if (!file) {
-			fprintf(stderr, "Could not open file: %s\n", argv[i]);
+			fprintf(stderr, "Could not open file %s: %s\n", argv[i], strerror(errno));
 			rc = SEPOL_ERR;
 			goto exit;
 		}
 		rc = stat(argv[i], &filedata);
 		if (rc == -1) {
-			fprintf(stderr, "Could not stat file: %s\n", argv[i]);
+			fprintf(stderr, "Could not stat file %s: %s\n", argv[i], strerror(errno));
 			rc = SEPOL_ERR;
 			goto exit;
 		}
@@ -288,7 +288,7 @@ int main(int argc, char *argv[])
 		buffer = malloc(file_size);
 		rc = fread(buffer, file_size, 1, file);
 		if (rc != 1) {
-			fprintf(stderr, "Failure reading file: %s\n", argv[i]);
+			fprintf(stderr, "Failure reading file %s: %s\n", argv[i], strerror(errno));
 			rc = SEPOL_ERR;
 			goto exit;
 		}
@@ -329,7 +329,7 @@ int main(int argc, char *argv[])
 		int size = snprintf(NULL, 0, "policy.%d", policyvers);
 		output = malloc((size + 1) * sizeof(char));
 		if (output == NULL) {
-			fprintf(stderr, "Failed to create output filename\n");
+			fprintf(stderr, "Failed to allocate output filename: %s\n", strerror(errno));
 			rc = SEPOL_ERR;
 			goto exit;
 		}
@@ -342,7 +342,7 @@ int main(int argc, char *argv[])
 
 	binary = fopen(output, "w");
 	if (binary == NULL) {
-		fprintf(stderr, "Failure opening binary file for writing\n");
+		fprintf(stderr, "Failure opening binary file %s for writing: %s\n", output, strerror(errno));
 		rc = SEPOL_ERR;
 		goto exit;
 	}
@@ -361,8 +361,13 @@ int main(int argc, char *argv[])
 		goto exit;
 	}
 
-	fclose(binary);
+	rc = fclose(binary);
 	binary = NULL;
+	if (rc != 0) {
+		fprintf(stderr, "Failed to close binary policy %s: %s\n", output, strerror(errno));
+		rc = SEPOL_ERR;
+		goto exit;
+	}
 
 	rc = cil_filecons_to_string(db, &fc_buf, &fc_size);
 	if (rc != SEPOL_OK) {
@@ -377,19 +382,29 @@ int main(int argc, char *argv[])
 	}
 
 	if (file_contexts == NULL) {
-		fprintf(stderr, "Failed to open file_contexts file\n");
+		fprintf(stderr, "Failed to open file_contexts file %s: %s\n",
+			filecontexts ? filecontexts : "file_contexts",
+			strerror(errno));
 		rc = SEPOL_ERR;
 		goto exit;
 	}
 
 	if (fwrite(fc_buf, sizeof(char), fc_size, file_contexts) != fc_size) {
-		fprintf(stderr, "Failed to write file_contexts file\n");
+		fprintf(stderr, "Failed to write file_contexts file %s\n",
+			filecontexts ? filecontexts : "file_contexts");
 		rc = SEPOL_ERR;
 		goto exit;
 	}
 
-	fclose(file_contexts);
+	rc = fclose(file_contexts);
 	file_contexts = NULL;
+	if (rc != 0) {
+		fprintf(stderr, "Failed to close file_contexts file %s: %s\n",
+			filecontexts ? filecontexts : "file_contexts",
+			strerror(errno));
+		rc = SEPOL_ERR;
+		goto exit;
+	}
 
 	rc = SEPOL_OK;
 
