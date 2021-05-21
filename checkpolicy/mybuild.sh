f66cceec059bd02a15b01299258535457bd6d594
Christian Göttsche <cgzones@googlemail.com>
2021-05-21 14:34:51 +0200

    add mybuild script
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

---
+++ checkpolicy/mybuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+if [ "$1x" = "cleanx" ]; then
+	make -C ../libsepol/src/ clean
+	make clean
+
+	exit 0
+fi
+
+CC=${CC:-'ccache clang-12'}
+CFLAGS=${CFLAGS:-'-O1 -g -fsanitize=address -fsanitize-address-use-after-scope -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=nullability -fsanitize=implicit-conversion -fsanitize=integer -fsanitize=float-divide-by-zero -fsanitize=local-bounds'}
+
+make -C ../libsepol/src/ CC="$CC" CFLAGS="$CFLAGS -Werror -Wall -Wdeclaration-after-statement -Wextra -Wfloat-equal -Wformat -Winit-self -Wmissing-format-attribute -Wmissing-noreturn -Wnull-dereference -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings" libsepol.a -j4
+
+make clean
+make CC="$CC" CFLAGS="$CFLAGS -Werror -Wall -Wcast-qual -Wdeclaration-after-statement -Wextra -Wfloat-equal -Wformat=2 -Winit-self -Wmissing-format-attribute -Wmissing-noreturn -Wmissing-prototypes -Wnull-dereference -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings -isystem $(pwd)/../libsepol/include/" LIBSEPOLA="$(pwd)/../libsepol/src/libsepol.a" -j4
