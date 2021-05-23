7d93ce84c62b642b34ee2d30cd386b4751463d9e
Christian Göttsche <cgzones@googlemail.com>
2021-05-23 20:26:53 +0200

    mybuild
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

--- checkpolicy/mybuild.sh
+++ checkpolicy/mybuild.sh
@@ -10,9 +10,9 @@ if [ "$1x" = "cleanx" ]; then
 fi
 
 CC=${CC:-'ccache clang-12'}
-CFLAGS=${CFLAGS:-'-O1 -g -fsanitize=address -fsanitize-address-use-after-scope -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=nullability -fsanitize=implicit-conversion -fsanitize=integer -fsanitize=float-divide-by-zero -fsanitize=local-bounds'}
+CFLAGS=${CFLAGS:-'-fsanitize=fuzzer -O1 -g -fsanitize=address -fsanitize-address-use-after-scope -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=nullability -fsanitize=implicit-conversion -fsanitize=integer -fsanitize=float-divide-by-zero -fsanitize=local-bounds'}
 
 make -C ../libsepol/src/ CC="$CC" CFLAGS="$CFLAGS -Werror -Wall -Wdeclaration-after-statement -Wextra -Wfloat-equal -Wformat -Winit-self -Wmissing-format-attribute -Wmissing-noreturn -Wnull-dereference -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings" libsepol.a -j4
 
 make clean
-make CC="$CC" CFLAGS="$CFLAGS -Werror -Wall -Wcast-qual -Wdeclaration-after-statement -Wextra -Wfloat-equal -Wformat=2 -Winit-self -Wmissing-format-attribute -Wmissing-noreturn -Wmissing-prototypes -Wnull-dereference -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings -isystem $(pwd)/../libsepol/include/" LIBSEPOLA="$(pwd)/../libsepol/src/libsepol.a" -j4
+make CC="$CC" CFLAGS="-DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION $CFLAGS -Werror -Wall -Wcast-qual -Wdeclaration-after-statement -Wextra -Wfloat-equal -Wformat=2 -Winit-self -Wmissing-format-attribute -Wmissing-noreturn -Wmissing-prototypes -Wnull-dereference -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings -isystem $(pwd)/../libsepol/include/" LIBSEPOLA="$(pwd)/../libsepol/src/libsepol.a" checkmodule-fuzzer
