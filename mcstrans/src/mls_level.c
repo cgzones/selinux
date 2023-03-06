e696ef84a75bb9d53d015920b62a0b188e5a6124
Christian Göttsche <cgzones@googlemail.com>
2023-03-06 21:38:50 +0100

    mcstrans: check for oom to avoid NULL dereference
        mls_level.c: In function ‘mls_level_to_string’:
        mls_level.c:129:14: warning: use of possibly-NULL ‘result’ where non-null expected [CWE-690] [-Wanalyzer-possible-null-argument]
          129 |         p += sprintf(p, "s%d", l->sens);
              |              ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    TODO: check in callers for NULL return from mls_level_to_string()
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

--- mcstrans/src/mls_level.c
+++ mcstrans/src/mls_level.c
@@ -130,6 +130,8 @@ char *mls_level_to_string(mls_level_t *l)
 	if (len == 0)
 		return NULL;
 	result = (char *)malloc(len + 1);
+	if (!result)
+		return NULL;
 	p = result;
 
 	p += sprintf(p, "s%d", l->sens);
