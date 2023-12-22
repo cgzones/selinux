6717aed8b345ed0d4569c9e330fdf96890758a46
Christian Göttsche <cgzones@googlemail.com>
2023-12-22 17:57:37 +0100

    fp casts
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

--- libsemanage/src/booleans_policydb.c
+++ libsemanage/src/booleans_policydb.c
@@ -18,6 +18,8 @@
 
 /* Copyright (C) 2005 Red Hat, Inc. */
 
+#include <threads.h>
+
 struct semanage_bool;
 struct semanage_bool_key;
 typedef struct semanage_bool record_t;
@@ -35,19 +37,55 @@ typedef struct dbase_policydb dbase_t;
 #include "database_policydb.h"
 #include "semanage_store.h"
 
+static int sepol_bool_set_wrapper(sepol_handle_t * handle,
+				  sepol_policydb_t * policydb,
+				  const record_key_t * key,
+				  const record_t * data)
+{
+	return sepol_bool_set(handle, policydb, (const sepol_bool_key_t *)key, (const sepol_bool_t *)data);
+}
+
+static int sepol_bool_exists_wrapper(sepol_handle_t * handle,
+			     	     const sepol_policydb_t * policydb,
+			   	     const record_key_t * key,
+				     int *response)
+{
+	return sepol_bool_exists(handle, policydb, (const sepol_bool_key_t *)key, response);
+}
+
+static int sepol_bool_query_wrapper(sepol_handle_t * handle,
+			    	    const sepol_policydb_t * p,
+			   	    const record_key_t * key,
+			 	    record_t ** response)
+{
+	return sepol_bool_query(handle, p, (const sepol_bool_key_t *)key, (sepol_bool_t **)response);
+}
+
+static thread_local int (*saved_fn) (const record_t * boolean, void *fn_arg) = NULL;
+
+static int callback_wrapper(const sepol_bool_t *boolean, void *arg) {
+	return saved_fn((const record_t *)boolean, arg);
+}
+
+static int sepol_bool_iterate_wrapper(sepol_handle_t * handle,
+				      const sepol_policydb_t * policydb,
+			  	      int (*fn) (const record_t * boolean, void *fn_arg),
+				      void *arg)
+{
+	saved_fn = fn;
+
+	return sepol_bool_iterate(handle, policydb, callback_wrapper, arg);
+}
+
 /* BOOLEAN RECRORD (SEPOL): POLICYDB extension: method table */
 static const record_policydb_table_t SEMANAGE_BOOL_POLICYDB_RTABLE = {
 	.add = NULL,
 	.modify = NULL,
-/* FIXME: these casts depend on structs in libsepol matching structs
- * in libsemanage. This is incredibly fragile - the casting gets
- * rid of warnings, but is not type safe.
- */
-	.set = (record_policydb_table_set_t) sepol_bool_set,
-	.query = (record_policydb_table_query_t) sepol_bool_query,
-	.count = (record_policydb_table_count_t) sepol_bool_count,
-	.exists = (record_policydb_table_exists_t) sepol_bool_exists,
-	.iterate = (record_policydb_table_iterate_t) sepol_bool_iterate,
+	.set = sepol_bool_set_wrapper,
+	.query = sepol_bool_query_wrapper,
+	.count = sepol_bool_count,
+	.exists = sepol_bool_exists_wrapper,
+	.iterate = sepol_bool_iterate_wrapper,
 };
 
 int bool_policydb_dbase_init(semanage_handle_t * handle,
